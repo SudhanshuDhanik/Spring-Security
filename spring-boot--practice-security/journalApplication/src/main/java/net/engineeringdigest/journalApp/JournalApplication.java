863dab18aae538df12320485eef3d4daa832923c
Sudhanshu Dhanik <sonudhanik2812@gmail.com>
2024-06-12 17:12:38 +0530

    spring-security code

---
+++ spring-boot--practice-security/journalApplication/src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -0,0 +1,27 @@
+package net.engineeringdigest.journalApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement //used to Find @Transactional
+public class JournalApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Apka Program Chalra ha");SpringApplication.run(JournalApplication.class, args);
+    }
+     //Interface name is PlatformTransactionManager which is implemented by
+         // MongoTransactionManager  . MongoDatabaseFactory is interface used to handle db transactions
+    @Bean
+    public PlatformTransactionManager kuchbhi(MongoDatabaseFactory dbFactory){
+        return new MongoTransactionManager(dbFactory);
+ }
+
+
+}
---
+++ spring-boot--practice-security/journalApplication/src/main/java/net/engineeringdigest/journalApp/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package net.engineeringdigest.journalApp.Repository;
+
+import net.engineeringdigest.journalApp.Entity.JournalEntry;
+import net.engineeringdigest.journalApp.Entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+
+    void deleteByUserName(String username);
+}
+//Controller -->  Service  --> Repository
